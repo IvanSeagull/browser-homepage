e9ea48260aa321d979830d530d00a7d3bd94b9bf
IvanSeagull <47693465+IvanSeagull@users.noreply.github.com>
2022-06-11 16:35:46 +0100

    Set up

---
+++ src/components/MainScreen/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./style.module.scss"
+
+const MainScreen = () => {
+  return (
+    <div className={styles.con}>MainScreen</div>
+  )
+}
+
+export default MainScreen
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from  "./style.module.scss"
+
+const Sidebar = () => {
+  return (
+    <div className={styles.con}>sidebar</div>
+  )
+}
+
+export default Sidebar
